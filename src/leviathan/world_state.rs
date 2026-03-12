d352092022c51bdc0c30a65d840a96154596917b
taku <nezu4917@gmail.com>
2026-03-13 00:15:19 +0900

    feat: WorldState, Address, Account構造体を作成した

---
+++ src/leviathan/world_state.rs
@@ -0,0 +1,17 @@
+#![allow(dead_code)]
+
+use primitive_types::U256;
+use std::collections::HashMap;
+use sha3::{Deccak256, Digest};
+
+pub struct WorldState(pub HashMap<Address, Account>);
+
+pub struct Address(pub [u8;20]);
+
+pub stcut Account {
+    pub nonce: u32,
+    pub balance: U256, 
+    pub storage: HashMap<U256, U256>,
+    pub code: Vec<u8>,
+}
+
