67e77b69ffcbd3837119af2c71e41e97a0123eff
taku <nezu4917@gmail.com>
2026-03-13 00:23:05 +0900

    feat: main.rs にleviathanモジュールを追加し，作成した構造体を読み込み． fix: Accout宣言のタイポを修正

--- src/leviathan/world_state.rs
+++ src/leviathan/world_state.rs
@@ -2,13 +2,13 @@
 
 use primitive_types::U256;
 use std::collections::HashMap;
-use sha3::{Deccak256, Digest};
+use sha3::{Keccak256, Digest};
 
 pub struct WorldState(pub HashMap<Address, Account>);
 
 pub struct Address(pub [u8;20]);
 
-pub stcut Account {
+pub struct Account {
     pub nonce: u32,
     pub balance: U256, 
     pub storage: HashMap<U256, U256>,
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![allow(dead_code)]
+pub mod leviathan;
+use crate::leviathan::world_state::{WorldState, Address, Account};
+
 fn main() {
     println!("Hello, world!");
 }
