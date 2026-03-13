2a6efcea8c70d25bfbb96003d1103e215f4e0f03
taku <nezu4917@gmail.com>
2026-03-13 21:51:56 +0900

    feat: ExecutionEnvironment, BlockHeader構造体を定義した

--- src/leviathan/structs.rs
+++ src/leviathan/structs.rs
@@ -2,6 +2,7 @@
 
 use crate::leviathan::world_state::{WorldState, Address, Account};
 use primitive_types::U256;
+use std::time::SystemTime;
 
 pub struct SubState {
     a_des: Vec<Address>,
@@ -18,3 +19,24 @@ pub struct Log {
     data: Vec<u8>,
 }
 
+pub struct ExecutionEnvironment {
+    i_address: Address,     //現在実行中のコードを所有しているアカウント
+    i_origin: Address,      //実行の起点となった大本のトランザクション送信者
+    i_gas_price: U256,      //この実行の起点となったトランザクションの署名者が支払うガス価格
+    i_data: Vec<u8>,        //実行への入力データ
+    i_sender: Address,      //このコードを実行する直接の原因となったアカウント
+    i_value: U256,          //実行に伴ってアカウントに渡される総金額
+    i_byte: Vec<u8>,        //実行されるマシンコードのバイト列
+    i_block_header: BlockHeader,    //現在のブロックヘッダー情報
+    i_depth: usize,
+    i_permission: bool,     //ステートを変更する権限の有無
+}
+
+pub struct BlockHeader {
+    h_beneficiary: Address,     //ブロックの優先手数料を受け取るアドレス
+    h_timestamp: SystemTime,    //ブロック生成時の妥当なUnixスタンプ:
+    h_number: U256,             //ブロックnumber
+    h_prevrandao: U256,         //前のブロックbいー今ステートから提供される乱数生成用の値
+    h_gaslimit: U256,           //ブロック全体のガス上限
+    h_basefee: U256,            //消費されたガス１単位あたりにバーンされるお金
+}
