4d23fcd35a39e29e89585c350371db7ce62d817f
taku <nezu4917@gmail.com>
2026-03-23 00:51:22 +0900

    feat: TransactionExecutionトレイト executionメソッドの実装 ・ステップ1 初期ガスの計算・事前支払いコストの計算を実装した

--- src/leviathan/leviathan.rs
+++ src/leviathan/leviathan.rs
@@ -1,9 +1,11 @@
 #![allow(dead_code)]
 
 use alloy_primitives::{I256, U256};
-use crate::my_trait::evm_trait::{Xi, Gfunction};
+use crate::my_trait::leviathan_trait::{State, TransactionExecution};
 use crate::leviathan::world_state::{WorldState, Address, Account};
-use crate::leviathan::structs::{SubState, ExecutionEnvironment, Transaction};
+use crate::leviathan::structs::{SubState, ExecutionEnvironment, Log, Transaction};
+use crate::evm::evm::EVM;
+use sha3::{Keccak256, Digest};
 
 #[derive(Debug,Clone)]
 pub enum Action {
@@ -18,4 +20,45 @@ pub enum Action {
 
 pub struct LEVIATHAN (Vec<Action>);
 
+impl TransactionExecution for LEVIATHAN {
+     fn execution(state: &mut WorldState, transaction:Transaction) -> Result<(U256, Vec<Log>, bool),(U256, Vec<Log>, bool)> {
+        //=======ステップ1===========
+        //初期ガスの計算
+        let base_gas = U256::from(21000);  //基本料金
+        let mut data_gas = U256::ZERO;
+        let mut index = 0;
+        while index < transaction.data.len() {  //データに関するガス
+            if transaction.data[index] == 0 {
+                data_gas = data_gas.saturating_add(U256::from(4));
+            }else{
+                data_gas = data_gas.saturating_add(U256::from(16));
+            }
+            index += 1;
+        }
+        let mut contract_gas = U256::ZERO;
+        if transaction.t_to.is_none() {     //コントラクト作成追加費
+            contract_gas = contract_gas.saturating_add(U256::from(32000));
+            let words = U256::from(transaction.data.len()).saturating_add(U256::from(31)) / U256::from(32);
+            let word_gas = words.saturating_mul(U256::from(2));
+            contract_gas = contract_gas.saturating_add(word_gas);
+        }
+
+        let all_gas = base_gas + data_gas + contract_gas;
+
+        //事前支払いコスト
+        let max_cost = transaction.t_gas_limit.saturating_mul(transaction.t_price) + transaction.t_value;
+
+        //トランザクションの事前検証
+
+        return Ok((U256::ZERO, Vec::new(), true));
+
+
+
+
+
+
+
+         
+     }
+}
 
--- src/leviathan/mod.rs
+++ src/leviathan/mod.rs
@@ -1,4 +1,5 @@
 pub mod world_state;
 pub mod structs;
 pub mod state_method;
+pub mod leviathan;
 
