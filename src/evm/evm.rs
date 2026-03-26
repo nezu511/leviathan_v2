9d5811c204db769c98e1cda90bf83f11d280abc1
taku <nezu4917@gmail.com>
2026-03-26 17:40:02 +0900

    save: vmTestsを全てpass

--- src/evm/evm.rs
+++ src/evm/evm.rs
@@ -117,7 +117,7 @@ mod tests {
     #[test]
     fn test_add_instructions() {
         // 前回作成していただいた ADD 専用のディレクトリを指定してください
-        let test_dir = "test_data/VMTests/vmPushDupSwapTest";
+        let test_dir = "test_data/VMTests/vmTests";
         
         let paths = fs::read_dir(test_dir).expect("ディレクトリの読み込みに失敗しました");
 
