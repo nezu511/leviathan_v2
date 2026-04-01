dd60960e623dff846bf5c13445b365b98c43edea
taku <nezu4917@gmail.com>
2026-04-01 16:52:33 +0900

    test: stInitCodeTestを全てLLLからバイトコードに訳した

--- src/leviathan/leviathan.rs
+++ src/leviathan/leviathan.rs
@@ -360,7 +360,7 @@ mod state_tests {
     #[test]
     fn state_test_all_in_dir() {
         // ここにテストしたいディレクトリへのパスを指定します
-          let test_dir = "testdata/GeneralStateTestsFiller/CompleteTest";
+          let test_dir = "testdata/GeneralStateTestsFiller/stInitCodeTest/byte/";
         //let test_dir = "testdata/GeneralStateTestsFiller/stCreateTest/byte";
         //let test_dir = "testdata/GeneralStateTestsFiller/stMemoryTest";
 
