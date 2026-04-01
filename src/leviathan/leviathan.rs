df4222ce3da3435a7acbda11534835a6d557b172
taku <nezu4917@gmail.com>
2026-04-01 21:14:02 +0900

    save: stBadOpcodeをpass

--- src/leviathan/leviathan.rs
+++ src/leviathan/leviathan.rs
@@ -357,8 +357,8 @@ mod state_tests {
     #[test]
     fn state_test_all_in_dir() {
         // ここにテストしたいディレクトリへのパスを指定します
-        //let test_dir = "testdata/GeneralStateTestsFiller/stInitCodeTest/byte/";
-        let test_dir = "testdata/GeneralStateTestsFiller/CompleteTest";
+        let test_dir = "testdata/GeneralStateTestsFiller/stBadOpcode";
+        //let test_dir = "testdata/GeneralStateTestsFiller/CompleteTest";
         //let test_dir = "testdata/GeneralStateTestsFiller/stMemoryTest";
 
         let paths = fs::read_dir(test_dir)
