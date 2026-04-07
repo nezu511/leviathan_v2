44cb73e082d611dd0f37e34df7bc7443ad79ce66
taku <nezu4917@gmail.com>
2026-04-08 02:08:19 +0900

    refactor: EVM.gasにおいてEXTCODESIZE及びBALANCEでフォークに沿ったガスコストを請求するように変更した

--- src/evm/gas.rs
+++ src/evm/gas.rs
@@ -209,12 +209,34 @@ impl Gfunction for EVM {
                 return total;
             }
 
-            0x31 | 0x3b => {
+            0x31  => {
                 //BALANCE
-                //Address型に変換
-                let data = self.peek(0);
-                let cost = self.is_account_access(data, substate);
-                return U256::from(cost);
+                if self.version < VersionId::TangerineWhistle {
+                    return U256::from(20);
+                }else if self.version < VersionId::Istanbul {
+                    return U256::from(400);
+                }else if self.version < VersionId::Berlin {
+                    return U256::from(700);
+                }else{
+                    //Address型に変換
+                    let data = self.peek(0);
+                    let cost = self.is_account_access(data, substate);
+                    return U256::from(cost);
+                }
+            }
+
+            0x3b => {
+                //EXTCODESIZE
+                if self.version < VersionId::TangerineWhistle {
+                    return U256::from(20);
+                }else if self.version < VersionId::Berlin {
+                    return U256::from(700);
+                }else{
+                    //Address型に変換
+                    let data = self.peek(0);
+                    let cost = self.is_account_access(data, substate);
+                    return U256::from(cost);
+                }
             }
 
             0x3f => {
