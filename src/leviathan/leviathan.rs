b87dff04a4821f0aa5ae6c4ea7ea4e5c72342dd0
taku <nezu4917@gmail.com>
2026-04-17 02:37:14 +0900

    save: stInitCodeTestもパスした

--- src/leviathan/leviathan.rs
+++ src/leviathan/leviathan.rs
@@ -593,7 +593,7 @@ mod state_tests {
             .try_init();
 
         // 対象のディレクトリ
-        let test_dir = "MPTTest/stWalletTest";
+        let test_dir = "MPTTest/stInitCodeTest";
 
         let paths = std::fs::read_dir(test_dir)
             .unwrap_or_else(|_| panic!("Failed to read test directory: {}", test_dir));
