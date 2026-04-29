2c737a5c37b16f64bc4ea2a81d76b2c38c97d70c
taku <nezu4917@gmail.com>
2026-04-29 22:51:50 +0900

    feat: LeviathanApp構造体を定義した． フィールドにLEVIATHAN, WorldStateを実装した．

--- src/bin/abci_node.rs
+++ src/bin/abci_node.rs
@@ -4,11 +4,32 @@ use tendermint_proto::abci::{
     ResponseCommit, ResponseFinalizeBlock, ResponseInfo,
 };
 use tracing::{info, Level};
+use std::sync::Arc;
+use std::sync::Mutex;
 
-#[derive(Default, Clone)]
-struct LeviathanMockApp;
+//自作構造体
+use leviathan_v2::leviathan::leviathan::LEVIATHAN;
+use leviathan_v2::leviathan::structs::VersionId;
+use leviathan_v2::leviathan::world_state::{Account, WorldState};
 
-impl Application for LeviathanMockApp {
+
+#[derive(Clone)]
+struct LeviathanApp {
+    state: Arc<Mutex<WorldState>>,
+    leviathan: Arc<Mutex<LEVIATHAN>>,
+}
+
+impl LeviathanApp {
+    pub fn new(version: VersionId) -> Self {
+        Self {
+            state: Arc::new(Mutex::new(WorldState::new())),
+            leviathan: Arc::new(Mutex::new(LEVIATHAN::new(version))),
+        }
+    }
+}
+
+
+impl Application for LeviathanApp {
     /// 1. Info: CometBFT起動時に呼ばれる状態同期
     fn info(&self, _req: RequestInfo) -> ResponseInfo {
         info!("[INFO] CometBFTからInfoリクエストを受信しました");
@@ -67,7 +88,7 @@ fn main() {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
     info!("Leviathan ABCI Mock Serverを起動中...");
 
-    let app = LeviathanMockApp::default();
+    let app = LeviathanApp::new(VersionId::Constantinople);
     
     let server = ServerBuilder::default()
         .bind("127.0.0.1:26658", app)
